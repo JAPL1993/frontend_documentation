d1aab879ffd3bb5f0f84e1809a730d264b29232e
JAPL1993 <108290394+JAPL1993@users.noreply.github.com>
2023-03-28 18:33:03 -0600

    breakpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Documentation';
   classes = ['example-container', 'rounded-xl'];
   isOpen: boolean = true;
-
+  constructor(private resposive: BreakpointObserver) {}
   statusSide() {
     this.isOpen = !this.isOpen;
     console.log(this.isOpen);
+  }
+  ngOnInit(): void {
+    this.resposive.observe(Breakpoints.Small).subscribe((result) => {
+      if (result.matches) {
+        this.isOpen = true;
+      }
+    });
   }
 }
